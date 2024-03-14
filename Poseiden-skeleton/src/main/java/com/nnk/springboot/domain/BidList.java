41f5a36fdce633bb492038696262a45226f1eddc
NdeyeHoudia <ndeyehoudiasylla02@gmail.com>
2024-03-14 14:14:52 +0100

    recupération du projet

--- Poseiden-skeleton/src/main/java/com/nnk/springboot/domain/BidList.java
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/domain/BidList.java
@@ -1,15 +1,44 @@
 package com.nnk.springboot.domain;
 
-import org.springframework.beans.factory.annotation.Required;
 
-import javax.persistence.*;
-import javax.validation.constraints.Digits;
-import javax.validation.constraints.NotBlank;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Digits;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
 import java.sql.Date;
 import java.sql.Timestamp;
 
+@Data
 @Entity
 @Table(name = "bidlist")
 public class BidList {
     // TODO: Map columns in data table BIDLIST with corresponding java fields
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer BidListId;
+    private String account;
+    private String type;
+    private Double bidQuantity;
+    private Double askQuantity;
+    private Double bid;
+    private Double ask;
+    private String benchmark;
+    private Timestamp bidListDate;
+    private String commentary;
+    private String security;
+    private String status;
+    private String trader;
+    private String book;
+    private String creationName;
+    private Timestamp creationDate;
+    private String revisionName;
+    private Timestamp revisionDate;
+    private String dealName;
+    private String dealType;
+    private String sourceListId;
+    private String side;
+
+
 }
--- Poseiden-skeleton/src/main/java/com/nnk/springboot/domain/CurvePoint.java
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/domain/CurvePoint.java
@@ -1,15 +1,24 @@
 package com.nnk.springboot.domain;
 
+import lombok.Data;
 import org.hibernate.validator.constraints.Length;
 
-import javax.persistence.*;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import java.sql.Timestamp;
 
-
+@Data
 @Entity
 @Table(name = "curvepoint")
 public class CurvePoint {
     // TODO: Map columns in data table CURVEPOINT with corresponding java fields
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Integer id;
+    Integer curveId;
+    Timestamp asOfDate;
+    Double term;
+    Double value;
+    Timestamp creationDate;
 }
